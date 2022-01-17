d79132ac02e03f7c1bcd4e68d5956b1c7ebd631f
DenisenkoDenis1990 <97170242+DenisenkoDenis1990@users.noreply.github.com>
2022-01-17 17:27:07 +0200

    Создание проекта
    Создание проекта в XCode, настройка UI и простановка constraints в сториборде

---
+++ 8-Ball Without Actual Ball/8-Ball Without Actual Ball/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  8-Ball Without Actual Ball
+//
+//  Created by Denys Denysenko on 10.01.2022.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+   
+
+
+}
+
